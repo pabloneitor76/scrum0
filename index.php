5d1d6a83587e4bac250fe20dffbcc51cf5d879e0
pabloneitor76 <pabloneitor_76@hotmail.com>
2018-04-07 08:36:01 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formularios</title>
+
+        <link type="text/css" rel="stylesheet" href="public/css/estilos.css"/>
+    </head>
+
+    <body>
+        <header>
+            <div class="wrapper">
+                <h1 class="logo"> PHP - B&aacute;sico</h1>
+
+                <nav>
+                    <a href="#">Inicio</a></li>
+                    <a href="views/login.php">Login</a></li>
+                </nav>
+            </div>
+        </header>
+
+        <section class="contenido wrapper">
+            <h1>Pagina de Inicio</h1>
+
+        </section>
+    </body>
+</html>
