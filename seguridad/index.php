ae557d87650bdf0ee3956e3371374cfa5292ac54
pabloneitor76 <pabloneitor_76@hotmail.com>
2018-04-08 18:09:42 -0400

    segundo commit

---
+++ seguridad/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulario </title>
+
+        <link type="text/css" rel="stylesheet" href="public/css/estilos.css"/>
+    </head>
+
+    <body>
+        <header>
+            <div class="wrapper">
+                <h1 class="logo"> PHP asknkasdlas- B&aacute;sico</h1>
+
+                <nav>
+                    <a href="#">Inicio limpieza</a></li>
+                    <a href="views/login.php">Login</a></li>
+                </nav>
+            </div>
+        </header>
+
+        <section class="contenido wrapper">
+            <h1>Pagina de Inicio 1</h1>
+
+        </section>
+    </body>
+</html>
